8d2e8f8fddd73811cc4503ab57265a36139d453c
beerser <bearjaiton@gmail.com>
2024-09-25 15:53:14 +0700

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,6 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
